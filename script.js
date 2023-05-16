4f190e8ccc079e7c6b8d1c5340dd321901202e61
Gopal Patel <130164161+Gopalptl1405@users.noreply.github.com>
2023-05-16 09:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 function threeSum(arr, target) {
 // write your code here
+	function threeSum(S, target) {
+ 
+		S.sort((a, b) => a - b); 
+		// sort the array in ascending order
+  let closestSum = Infinity; 
+		// initialize closest sum to a large value
+  for (let i = 0; i < S.length - 2; i++) {
+    let left = i + 1; 
+	  // initialize left pointer
+    let right = S.length - 1;
+	  // initialize right pointer
+    while (left < right) {
+      const sum = S[i] + S[left] + S[right]; 
+		// calculate sum of current triplet
+      if (Math.abs(target - sum) < Math.abs(target - closestSum)) {
+        closestSum = sum; 
+		  // update closest sum if current sum is closer to target
+      }
+      if (sum < target) {
+        left++; 
+		  // move left pointer to increase sum
+      } else {
+        right--; 
+		  // move right pointer to decrease sum
+      }
+    }
+  }
+  return closestSum; // return closest sum found
+}
+
   
 }
 
